55026c0c2152d343b5b57b7e0490e27ec0150175
Denis Perov <denis.perov@gmail.com>
2025-07-28 23:45:53 +0300

    feat: Setup NPM workspaces

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['opa-builder'],
+  //transpilePackages: ['opa-builder'],
   experimental: {
     esmExternals: true,
   },
+  // turbopack: {
+  //   root: '..',
+  //   resolveAlias: {
+  //     'opa-builder': 'builder/src/index.js',
+  //   },
+  // },
 };
 
 export default nextConfig;
