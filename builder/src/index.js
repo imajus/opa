4efc4ad6fb131e55421b70e94cc8df891740c7ae
Denis Perov <denis.perov@gmail.com>
2025-07-28 22:09:09 +0300

    Restructure projects

--- builder/src/index.js
+++ builder/src/index.js
@@ -1,26 +1,18 @@
 /**
  * 1inch Limit Order Protocol Extension Wrappers
- * 
+ *
  * This package provides JavaScript wrappers for 1inch Limit Order Protocol extensions,
  * enabling easy creation and configuration of sophisticated trading strategies.
  */
 
 // Extension wrappers
-export { default as gasStation, GasStationConfigSchema } from './extensions/gas-station.js';
-export { 
-  default as chainlinkCalculator,
-  SingleOracleConfigSchema,
-  DoubleOracleConfigSchema, 
-  ChainlinkConfigSchema 
-} from './extensions/chainlink-calculator.js';
-export { default as dutchAuctionCalculator, DutchAuctionConfigSchema } from './extensions/dutch-auction-calculator.js';
-export { default as rangeAmountCalculator, RangeAmountConfigSchema } from './extensions/range-amount-calculator.js';
+export { default as gasStation } from './extensions/gas-station.js';
+export { default as chainlinkCalculator } from './extensions/chainlink-calculator.js';
+export { default as dutchAuctionCalculator } from './extensions/dutch-auction-calculator.js';
+export { default as rangeAmountCalculator } from './extensions/range-amount-calculator.js';
 
 // Utilities
 export { createWrapper } from './extensions/utils/factory.js';
-
-// Common schemas for reuse
-export * as schemas from './schemas/common.js';
 
 // Constants
 export { HookType, ALL_HOOK_TYPES } from './constants.js';
@@ -32,29 +24,5 @@ export const extensions = {
   gasStation: gasStation,
   chainlinkCalculator: chainlinkCalculator,
   dutchAuctionCalculator: dutchAuctionCalculator,
-  rangeAmountCalculator: rangeAmountCalculator
+  rangeAmountCalculator: rangeAmountCalculator,
 };
-
-/**
- * Extension wrapper metadata for discovery
- */
-export const extensionMeta = {
-  gasStation: gasStation.meta,
-  chainlinkCalculator: chainlinkCalculator.meta,
-  dutchAuctionCalculator: dutchAuctionCalculator.meta,
-  rangeAmountCalculator: rangeAmountCalculator.meta
-};
-
-/**
- * Extension-specific schemas for external validation
- */
-export const extensionSchemas = {
-  gasStation: GasStationConfigSchema,
-  chainlinkCalculator: {
-    single: SingleOracleConfigSchema,
-    double: DoubleOracleConfigSchema,
-    main: ChainlinkConfigSchema
-  },
-  dutchAuctionCalculator: DutchAuctionConfigSchema,
-  rangeAmountCalculator: RangeAmountConfigSchema
-}; 
