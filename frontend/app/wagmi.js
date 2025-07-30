b68e0ff6cbe3998e158c38fb07afb1fd209e538e
Denis Perov <denis.perov@gmail.com>
2025-07-30 09:42:18 +0300

    feat: add localhost network configuration and update app name in wagmi.js
    - Introduced a custom localhost network configuration for local development.
    - Updated the app name from 'My RainbowKit App' to 'OPA'.
    - Adjusted the chains array to include the new localhost configuration instead of multiple networks.
    
    Related to T4.0 in PRD

--- frontend/app/wagmi.js
+++ frontend/app/wagmi.js
@@ -1,18 +1,34 @@
 'use client';
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-  sepolia,
-} from 'wagmi/chains';
+import { mainnet } from 'wagmi/chains';
+
+// Custom localhost network configuration
+const localhost = {
+  id: 31337,
+  name: 'localhost',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://localhost:8545'],
+    },
+    public: {
+      http: ['http://localhost:8545'],
+    },
+  },
+  // blockExplorers: {
+  //   default: { name: 'Explorer', url: 'http://localhost:8545' },
+  // },
+  testnet: true,
+};
 
 export const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
+  appName: 'OPA',
   projectId: 'YOUR_PROJECT_ID', // Get from https://cloud.walletconnect.com
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [mainnet, localhost],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
